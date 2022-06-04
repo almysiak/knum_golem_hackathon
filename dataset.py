f1f8e36f81541d1a6d1aee60274af7621d9dabe6
Mateusz Blajda <mati99.b@gmail.com>
2022-06-04 17:41:45 +0200

    dataset for contrastive learning

---
+++ dataset.py
@@ -0,0 +1,48 @@
+import torch
+
+class ContrastiveLearningDataset(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        images,
+        labels,
+        transforms,
+    ):
+        self.transforms = transforms
+        self.images = images
+        self.labels = labels
+
+        self.prior_len = len(self.images)
+        self.prod_len = self.prior_len ** 2
+        
+        self.classes_val_count = labels.value_counts()
+        
+        self.pairs = []
+        self.weights = []
+        
+
+    def __getitem__(self, idx):
+        id_1 = idx // self.prior_len
+        id_2 = idx % self.prior_len
+        
+        image_1, label_1 = self.transforms(
+            self.images[id_1],
+            self.labels[id_1]
+        )
+        
+        image_2, label_2 = self.transforms(
+            self.images[id_2],
+            self.labels[id_2]
+        )
+        
+        sign = 1 if label_1 == label_2 else -1
+        weight = sign * (self.classes_val_count[label_1] * self.classes_val_count[label_2]) / self.prod_len
+        
+        return (
+            image_1,
+            image_2,
+            weight
+        )
+
+    def __len__(self):
+        return self.prod_len
+
