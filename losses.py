495cc3b323e0d08c7a50d2db6b34f0e004b13609
Mateusz Blajda <mati99.b@gmail.com>
2022-06-04 21:46:32 +0200

    losses

---
+++ losses.py
@@ -0,0 +1,34 @@
+import torch
+
+class ContrastiveLoss(torch.nn.Module):
+    def __init__(self, eps, reduction="mean"):
+        super.__init__()
+        self.eps = eps
+        self.reduction = reduction
+    def forward(self, y_1, y_2, sign, weight=None):
+        same = (sign + 1) // 2
+        sqr_dst = ((y_1 - y_2)**2).sum(dim=-1)
+        loss = same * sqr_dst + (1 - same) * torch.clip(self.eps - torch.sqrt(sqr_dst) , min=0) ** 2
+        if weight is not None:
+            loss = loss * weight
+        if self.reduction == "mean":
+            return loss.mean()
+        elif self.reduction == "sum":
+            return loss.sum()
+        return loss
+    
+class CosineSimilarityLoss(torch.nn.Module):
+    def __init__(self, eps=1e-6, reduction="mean"):
+        super.__init__()
+        self.cos_sim = torch.nn.CosineSimilarity(dim=1, eps=eps)
+        self.reduction = reduction
+    def forward(self, y_1, y_2, sign, weight=None):
+        cs = self.cos_sim(y_1, y_2)
+        loss = - cs * sign
+        if weight is not None:
+            loss = loss * weight
+        if self.reduction == "mean":
+            return loss.mean()
+        elif self.reduction == "sum":
+            return loss.sum()
+        return loss
