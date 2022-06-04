ec7202a720559ff60e698b2ec75681b55b17593a
Aleksandra Mysiak <al.mysiak@gmail.com>
2022-06-04 21:47:45 +0200

    Added resize to augmentations

--- transforms.py
+++ transforms.py
@@ -4,9 +4,28 @@ from torchvision.transforms import transforms as T, functional as F
 
 class ResizeToSquare(torch.nn.Module):
     def __init__(self, scale):
-        super.__init__()
+        super().__init__()
         self.scale = scale
+        
     def forward(self, x):
+        x.thumbnail([self.scale, self.scale])
+        x = T.ToTensor()(x)
+
+        # pad with zeros to shape (3, scale, scale)
+        pad = torch.zeros((3, self.scale - x.shape[1], x.shape[2]))
+        # print(x.shape, pad.shape)
+
+        x = torch.cat([x, pad], dim=1)
+        pad = torch.zeros((3, self.scale, self.scale - x.shape[2]))
+        # print(x.shape, pad.shape)
+
+        x = torch.cat([x, pad], dim=2)
+        x = (255 * x).to(torch.uint8)
+        # print(x.shape)
+        return x
+        
+        
+        
         
 
 train_transforms = T.Compose([
@@ -15,7 +34,7 @@ train_transforms = T.Compose([
     T.RandomHorizontalFlip(p=0.5),
     T.RandomVerticalFlip(p=0.5),
     T.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.1, hue=0.05),
-    T.ToTensor()
+    ResizeToSquare(224)
 ])
 
 
