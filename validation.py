755c4057b51e8932b34586b940fc4dd60ff0b973
Aleksandra Mysiak <al.mysiak@gmail.com>
2022-06-05 13:07:19 +0200

    validation with augmentations, not working

--- validation.py
+++ validation.py
@@ -3,6 +3,12 @@ import json
 import torch
 from torchvision.transforms import transforms as T
 from tqdm.notebook import tqdm
+from transforms import train_transforms, val_transforms
+from dataset import SimpleDataset
+
+BATCH_SIZE = 8
+
+NO_PASSES = 3
 
 def predict(model, ref_data, valid_data, dist_func, feature_extractor=None):
     cos = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
@@ -37,6 +43,60 @@ def predict(model, ref_data, valid_data, dist_func, feature_extractor=None):
                                 
             
         valid_data.loc[id_, "pred_category"] = ref_data.category_id[best_row]
+        
+    return valid_data
+
+def predict_knn_zoo(model, ref_data, valid_data, k=3, feature_extractor=None, final=False):
+    train_images = ref_data['img']
+    train_labels = ref_data['category_id']
+    train_dataset = SimpleDataset(train_images, train_labels, train_transforms)
+    
+    train_loader = torch.utils.data.DataLoader(
+    train_dataset,
+    BATCH_SIZE, 
+    shuffle=True, 
+    num_workers=0,
+    pin_memory=False,
+    )
+    
+    all_labels = []
+    all_ys = []
+    
+    for _ in range(NO_PASSES):
+        for i, data in enumerate(train_loader, 0):
+            torch.cuda.empty_cache()
+            images, labels = data
+            images = images.to(device=DEVICE)
+            images = [np.array(image.cpu(), dtype=np.uint8) for image in images]
+
+            ys = model(**feature_extractor(images, return_tensors='pt'))
+
+            all_ys.append(ys)
+            all_labels.append(labels)
+
+            
+    ref_vects = all_ys
+    
+    for id_, row in tqdm(valid_data.iterrows()):
+        if feature_extractor is not None:
+            vect = model(**feature_extractor(to_tensor(row["img"]), return_tensors='pt'))
+        else:
+            vect = model(to_tensor(row["img"]))
+            
+        dists = ((ref_vects - vect)**2).sum(dim=1)
+        k_nearest = dists.argsort()[:k]
+        classes = [all_labels[idx] for idx in k_nearest]
+        
+        def most_common(lst):
+            return max(set(lst), key=lst.count)
+        
+        class_ = most_common(classes)
+
+                                
+        if not final:
+            valid_data.loc[id_, "pred_category"] = class_
+        else:
+            valid_data.loc[id_, "category_id"] = class_
         
     return valid_data
             
