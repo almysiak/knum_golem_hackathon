f718636156e60f7de750b2a41f826e633744c3aa
Mateusz Blajda <mati99.b@gmail.com>
2022-06-05 04:14:24 +0200

    4am update

--- dataset.py
+++ dataset.py
@@ -1,5 +1,62 @@
 import torch
+import numpy as np
 
+def list_resize(l, s):
+    list_len = len(l)
+    reps = s // list_len
+    rest = s - reps * list_len
+    res = l * reps + l[:rest]
+    return res
+     
+    
+
+# class ContrastiveLearningDataset(torch.utils.data.Dataset):
+#     def __init__(
+#         self,
+#         images,
+#         labels,
+#         transforms,
+#     ):
+#         self.transforms = transforms
+#         self.images = images
+#         self.labels = labels
+
+#         self.prior_len = len(self.images)
+#         self.prod_len = self.prior_len ** 2
+        
+#         self.classes_val_count = labels.value_counts()
+        
+#         self.pairs = []
+#         self.weights = []
+        
+
+#     def __getitem__(self, idx):
+#         id_1 = idx // self.prior_len
+#         id_2 = idx % self.prior_len
+        
+#         image_1 = self.transforms(
+#             self.images[id_1]
+#         )
+#         label_1 = self.labels[id_1]
+        
+#         image_2 = self.transforms(
+#             self.images[id_2]
+#         )
+#         label_2 = self.labels[id_2]
+        
+#         sign = 1 if label_1 == label_2 else -1
+#         weight = self.prior_len / (self.classes_val_count[label_1] * self.classes_val_count[label_2])
+        
+#         return (
+#             image_1,
+#             image_2,
+#             sign,
+#             weight
+#         )
+
+#     def __len__(self):
+#         return self.prod_len
+    
 class ContrastiveLearningDataset(torch.utils.data.Dataset):
     def __init__(
         self,
@@ -8,44 +65,38 @@ class ContrastiveLearningDataset(torch.utils.data.Dataset):
         transforms,
     ):
         self.transforms = transforms
-        self.images = images
-        self.labels = labels
 
-        self.prior_len = len(self.images)
-        self.prod_len = self.prior_len ** 2
+        classes_val_count = labels.value_counts()
+        images_by_class = {}
         
-        self.classes_val_count = labels.value_counts()
+        for i, l in zip(images, labels):
+            if l not in images_by_class.keys():
+                images_by_class[l] = [i]
+            else:
+                images_by_class[l].append(i)
         
-        self.pairs = []
-        self.weights = []
+        max_count = classes_val_count.max()
+        for l in images_by_class.keys():
+            images_by_class[l] = list_resize(images_by_class[l], max_count)
+            
+        self.images = []
+        self.labels = []
         
+        for l in images_by_class.keys():
+            self.images = self.images + images_by_class[l]
+            self.labels = self.labels + [l] * max_count
+        
+        assert len(self.images) == len(self.labels)
 
     def __getitem__(self, idx):
-        id_1 = idx // self.prior_len
-        id_2 = idx % self.prior_len
-        
-        image_1 = self.transforms(
-            self.images[id_1]
-        )
-        label_1 = self.labels[id_1]
-        
-        image_2 = self.transforms(
-            self.images[id_2]
-        )
-        label_2 = self.labels[id_2]
-        
-        sign = 1 if label_1 == label_2 else -1
-        weight = self.prior_len / (self.classes_val_count[label_1] * self.classes_val_count[label_2])
-        
+        image = self.transforms(self.images[idx])
+        label = self.labels[idx]
         return (
-            image_1,
-            image_2,
-            sign,
-            weight
+            image,
+            label
         )
-
     def __len__(self):
-        return self.prod_len
+        return len(self.labels)
     
 class SimpleDataset(torch.utils.data.Dataset):
     def __init__(
--- losses.py
+++ losses.py
@@ -1,4 +1,5 @@
 import torch
+from itertools import combinations
 
 class ContrastiveLoss(torch.nn.Module):
     def __init__(self, eps, reduction="mean"):
@@ -6,31 +7,66 @@ class ContrastiveLoss(torch.nn.Module):
         self.eps = eps
         self.reduction = reduction
         
-    def forward(self, y_1, y_2, sign, weight=None):
-        same = (sign + 1) / 2
-        sqr_dst = ((y_1 - y_2)**2).sum(dim=-1)
-        loss = same * sqr_dst + (1 - same) * torch.clip(self.eps - torch.sqrt(sqr_dst) , min=0) ** 2
-        if weight is not None:
-            loss = loss * weight
+    def forward(self, ys, labels):
+        loss = torch.zeros(1).to(ys)
+        for (y1, l1), (y2, l2) in combinations(zip(ys, labels), 2):
+            sqr_dst = ((y1 - y2)**2).sum(dim=-1)
+            loss += sqr_dst if l1 == l2 else torch.clip(self.eps - torch.sqrt(sqr_dst) , min=0) ** 2
         if self.reduction == "mean":
-            return loss.mean()
-        elif self.reduction == "sum":
-            return loss.sum()
-        return loss
-    
+            return loss / len(labels)
+        else:
+            return loss
+        
+        
 class CosineSimilarityLoss(torch.nn.Module):
-    def __init__(self, eps=1e-6, reduction="mean"):
+    def __init__(self, eps=1e-6, reduction="mean", pos_weight=1.):
         super().__init__()
-        self.cos_sim = torch.nn.CosineSimilarity(dim=1, eps=eps)
+        self.pos_weight = pos_weight
+        self.cos_sim = torch.nn.CosineSimilarity(dim=-1, eps=eps)
         self.reduction = reduction
         
-    def forward(self, y_1, y_2, sign, weight=None):
-        cs = self.cos_sim(y_1, y_2)
-        loss = - cs * sign
-        if weight is not None:
-            loss = loss * weight
+    def forward(self, ys, labels):
+        loss = torch.zeros(1).to(ys)
+        for (y1, l1), (y2, l2) in combinations(zip(ys, labels), 2):
+            cs = self.cos_sim(y1, y2)
+            loss += cs * (-self.pos_weight if l1 == l2 else 1)
         if self.reduction == "mean":
-            return loss.mean()
-        elif self.reduction == "sum":
-            return loss.sum()
-        return loss
+            return loss / len(labels)
+        else:
+            return loss
+
+
+# class ContrastiveLoss(torch.nn.Module):
+#     def __init__(self, eps, reduction="mean"):
+#         super().__init__()
+#         self.eps = eps
+#         self.reduction = reduction
+        
+#     def forward(self, y_1, y_2, sign, weight=None):
+#         same = (sign + 1) / 2
+#         sqr_dst = ((y_1 - y_2)**2).sum(dim=-1)
+#         loss = same * sqr_dst + (1 - same) * torch.clip(self.eps - torch.sqrt(sqr_dst) , min=0) ** 2
+#         if weight is not None:
+#             loss = loss * weight
+#         if self.reduction == "mean":
+#             return loss.mean()
+#         elif self.reduction == "sum":
+#             return loss.sum()
+#         return loss
+    
+# class CosineSimilarityLoss(torch.nn.Module):
+#     def __init__(self, eps=1e-6, reduction="mean"):
+#         super().__init__()
+#         self.cos_sim = torch.nn.CosineSimilarity(dim=1, eps=eps)
+#         self.reduction = reduction
+        
+#     def forward(self, y_1, y_2, sign, weight=None):
+#         cs = self.cos_sim(y_1, y_2)
+#         loss = - cs * sign
+#         if weight is not None:
+#             loss = loss * weight
+#         if self.reduction == "mean":
+#             return loss.mean()
+#         elif self.reduction == "sum":
+#             return loss.sum()
+#         return loss
