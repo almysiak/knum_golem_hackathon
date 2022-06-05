0b2b495af9d3598125292e39559defc877f07a0a
Mateusz Blajda <mati99.b@gmail.com>
2022-06-05 14:24:43 +0200

    predict_knn_zoo fixed

--- validation.py
+++ validation.py
@@ -4,9 +4,12 @@ import torch
 from torchvision.transforms import transforms as T
 from tqdm.notebook import tqdm
 from transforms import train_transforms, val_transforms
-from dataset import SimpleDataset
+from dataset import SimpleDataset, ContrastiveLearningDataset
+import numpy as np
 
 BATCH_SIZE = 8
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 NO_PASSES = 3
 
@@ -49,7 +52,8 @@ def predict(model, ref_data, valid_data, dist_func, feature_extractor=None):
 def predict_knn_zoo(model, ref_data, valid_data, k=3, feature_extractor=None, final=False):
     train_images = ref_data['img']
     train_labels = ref_data['category_id']
-    train_dataset = SimpleDataset(train_images, train_labels, train_transforms)
+    train_dataset = ContrastiveLearningDataset(train_images, train_labels, val_transforms)
+    # train_dataset = SimpleDataset(train_images, train_labels, train_transforms)
     
     train_loader = torch.utils.data.DataLoader(
     train_dataset,
@@ -58,47 +62,50 @@ def predict_knn_zoo(model, ref_data, valid_data, k=3, feature_extractor=None, fi
     num_workers=0,
     pin_memory=False,
     )
-    
+    to_tensor = T.ToTensor()
     all_labels = []
     all_ys = []
-    
-    for _ in range(NO_PASSES):
-        for i, data in enumerate(train_loader, 0):
-            torch.cuda.empty_cache()
-            images, labels = data
-            images = images.to(device=DEVICE)
-            images = [np.array(image.cpu(), dtype=np.uint8) for image in images]
+    model.eval()
+    with torch.no_grad():
+        for _ in range(NO_PASSES):
+            for i, data in enumerate(train_loader, 0):
+                torch.cuda.empty_cache()
+                images, labels = data
+                images = images.to(device=DEVICE)
+                images = [np.array(image.cpu(), dtype=np.uint8) for image in images]
 
-            ys = model(**feature_extractor(images, return_tensors='pt'))
+                ys = model(**feature_extractor(images, return_tensors='pt').to(DEVICE))
 
-            all_ys.append(ys)
-            all_labels.append(labels)
+                all_ys.append(ys)
+                all_labels.append(labels)
 
+        all_labels = torch.cat(all_labels)
+        ref_vects = torch.cat(all_ys).to(DEVICE)
+        
+        for id_, row in tqdm(valid_data.iterrows()):
+            if feature_extractor is not None:
+                vect = model(**feature_extractor(to_tensor(row["img"]), return_tensors='pt').to(DEVICE))
+            else:
+                vect = model(to_tensor(row["img"]).to(DEVICE))
             
-    ref_vects = all_ys
-    
-    for id_, row in tqdm(valid_data.iterrows()):
-        if feature_extractor is not None:
-            vect = model(**feature_extractor(to_tensor(row["img"]), return_tensors='pt'))
-        else:
-            vect = model(to_tensor(row["img"]))
             
-        dists = ((ref_vects - vect)**2).sum(dim=1)
-        k_nearest = dists.argsort()[:k]
-        classes = [all_labels[idx] for idx in k_nearest]
-        
-        def most_common(lst):
-            return max(set(lst), key=lst.count)
-        
-        class_ = most_common(classes)
+            dists = ((ref_vects - vect)**2).sum(dim=1)
+            k_nearest = dists.argsort()[:k]
+            classes = [all_labels[idx] for idx in k_nearest]
+            
+            def most_common(lst):
+                return max(set(lst), key=lst.count)
+            
+            class_ = most_common(classes)
+            class_ = class_.cpu().numpy()
 
-                                
-        if not final:
-            valid_data.loc[id_, "pred_category"] = class_
-        else:
-            valid_data.loc[id_, "category_id"] = class_
+                                    
+            if not final:
+                valid_data.loc[id_, "pred_category"] = class_
+            else:
+                valid_data.loc[id_, "category_id"] = class_
         
-    return valid_data
+        return valid_data
             
     
 def predict_knn(model, ref_data, valid_data, k=3, feature_extractor=None, final=False):
